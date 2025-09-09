8f9c325a5e4b5ecb5c2134daef107508b9f98f60
Alexander Krylov <saskrkr123.com@mail.ru>
2025-09-09 20:34:55 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <div>
+          <h2>Баланс+</h2>
+        </div>
+      </header>
     </div>
   );
 }
